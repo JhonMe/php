741465502b7c50778ede8655e9f061c3855c4513
JhonMe <1386715@senati.pe>
2022-11-04 18:28:10 -0500

    cambio10

--- resources/views/huallay.blade.php
+++ resources/views/huallay.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,36 +9,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <style>
-        div{
+        body {
+            background: black;
+        }
+
+        h1 {
+            color: white;
+        }
+
+        div {
             height: 800px;
             width: 850px;
         }
+
+        p {
+            color: wheat;
+
+        }
     </style>
 </head>
+
 <body>
     <center>
-    <h1>Algunas muestras del lugar</h1>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://toposperu.files.wordpress.com/2008/08/00963.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRxGOxnc_f_zxhdYdo0x92uvJo-WQGTEec1yuHgkxUazcUhlTXCRDaWzsXsczJ71yrHxg&usqp=CAU" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://portal.andina.pe/EDPFotografia3/thumbnail/2017/07/24/000437507M.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</center>
+        <h1>Algunas muestras del lugar</h1>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://toposperu.files.wordpress.com/2008/08/00963.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRxGOxnc_f_zxhdYdo0x92uvJo-WQGTEec1yuHgkxUazcUhlTXCRDaWzsXsczJ71yrHxg&usqp=CAU" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://portal.andina.pe/EDPFotografia3/thumbnail/2017/07/24/000437507M.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div>
+            <p>
+                El Bosque de piedras de Huayllay está ubicado en el distrito de Huayllay, provincia y departamento de Pasco, Perú tiene una extensión de 6000 ha. ​ Está protegido
+                desde 1974 por el establecimiento del Santuario nacional de Huayllay. Constituye una maravilla natural andina, a 4,310 metros sobre el nivel del mar
+            </p>
+        </div>
+    </center>
 </body>
+
 </html>
